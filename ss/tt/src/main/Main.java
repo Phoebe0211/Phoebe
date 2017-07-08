ac369305676a4f24af17fc19812d9811e718b23c
zjj <1106490195@qq.com>
2017-07-08 14:05:17 +0800

    test

---
+++ ss/tt/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * Created by Administrator on 2017/7/8 0008.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+
+}

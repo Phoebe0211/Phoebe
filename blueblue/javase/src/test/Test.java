2a005e4732a7c4e6e670ee269c242b80d6af52e4
zjj <1106490195@qq.com>
2017-07-08 14:18:03 +0800

    test for github

---
+++ blueblue/javase/src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Administrator on 2017/7/8 0008.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+
+}
